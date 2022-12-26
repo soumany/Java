59f0c42352878f0f1b46dbab70d13ef439f2fff8
soumany <soumany06@gmail.com>
2022-12-26 15:27:02 +0700

    commit

--- HW1/lib/src/HW3.java
+++ HW1/lib/src/HW3.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 class HW3 {
     public static void main(String[] args){
-        //Enter a first number
+        //User input a frist value
         Scanner input1 = new Scanner(System.in);
         System.out.print("Enter a number :");
         int num1 = input1.nextInt();
-        int total = num1;
+        System.out.println("Your value is " + num1);
+
         System.out.print("Do you want to add another number? Y/N");
 
+         //Declare variable 
         char again = 'Y';
-       // String again = input1.nextLine();
-
+        //when user want to add another value
         while ( again == 'Y' ) {
             Scanner input2 = new Scanner(System.in);
             System.out.print("Enter another number:");
             int num2 = input2.nextInt();
-            total = total + num2;
+            int total = num1 + num2;
+            System.out.println("Your Total is" + total);
         }
-        System.out.println("Your Total is" + total);
-        System.out.print("Do you want to add another number? Y/N");
+        
+       // System.out.print("Do you want to add another number? Y/N");
 
 
         
