5b313bc0ef5c50a9599b2f7a1d2c3327ac919dc0
soumany <soumany06@gmail.com>
2023-03-08 15:55:56 +0700

    commit

--- Chapter04_User_Defined_Method/Exercise01.java
+++ Chapter04_User_Defined_Method/Exercise01.java
@@ -1,5 +1,18 @@
 public class Exercise01 {
 
+   
+    static boolean displayTwinPrime(int num) {
+  
+      if (num < 2)
+        return false;
+  
+      for (int i = 2; i <= num / 2; i++) {
+  
+        if (num % i == 0)
+          return false;
+      }
+      return true;
+    }
     public static void main(String[] args) {
   
       for (int i = 2; i < 1200; i++) {
@@ -10,17 +23,5 @@ public class Exercise01 {
       }
     }
   
-    static boolean displayTwinPrime(long n) {
-  
-      if (n < 2)
-        return false;
-  
-      for (int i = 2; i <= n / 2; i++) {
-  
-        if (n % i == 0)
-          return false;
-      }
-      return true;
-    }
   
  }
